11e774d1d59dd83b79a7180f88978d349fcefac7
msolorio <msolorio.github@gmail.com>
2021-01-20 10:50:14 -0800

    Adds starter code

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const app = express();
 const PORT = 4000;
-
 const usersController = require('./controllers/usersController');
 
+
+////////////////////////////////////////////////////////
 // Configuration / Middleware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'milo the barking dog',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7 * 2
-  }
-}));
 
+
+/////////////////////////////////////////////////////////
 // Controller
 app.use('/users', usersController);
+
+
+/////////////////////////////////////////////////////////
+// ROUTES
+app.get('/items', (req, res) => {
+  res.render('items/browseItems');
+})
+
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 
+/////////////////////////////////////////////////////////
+// START THE SERVER
 app.listen(PORT, () => {
   console.log(`Your app is listening on PORT: ${PORT}`);
 });
